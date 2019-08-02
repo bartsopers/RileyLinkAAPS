bb0c65d2edafb7208774218ade5f865a327fe711
Bart Sopers <bart@sopers.nl>
2019-08-02 18:06:01 +0200

    Lower basals in stub basal schedule

--- app/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/OmnipodManager.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/OmnipodManager.java
@@ -143,29 +143,29 @@ public class OmnipodManager {
 
     private BasalSchedule createStubBasalSchedule() {
         List<BasalScheduleEntry> basals = new ArrayList<>();
-        basals.add(new BasalScheduleEntry(4, Duration.standardMinutes(0)));    //00:00-00:30
-        basals.add(new BasalScheduleEntry(10, Duration.standardMinutes(30)));   //00:30-01:00
+        basals.add(new BasalScheduleEntry(0.55, Duration.standardMinutes(0)));    //00:00-00:30
+        basals.add(new BasalScheduleEntry(2.05, Duration.standardMinutes(30)));   //00:30-01:00
         basals.add(new BasalScheduleEntry(1, Duration.standardMinutes(60)));    //01:00-01:30
-        basals.add(new BasalScheduleEntry(11.5, Duration.standardMinutes(90))); //01:30-02:00
+        basals.add(new BasalScheduleEntry(1.3, Duration.standardMinutes(90))); //01:30-02:00
         basals.add(new BasalScheduleEntry(2, Duration.standardMinutes(120)));    //20:00-02:30
-        basals.add(new BasalScheduleEntry(12.5, Duration.standardMinutes(150))); //02:30-03:00
-        basals.add(new BasalScheduleEntry(3, Duration.standardMinutes(180)));    //03:00-03:30
-        basals.add(new BasalScheduleEntry(13.5, Duration.standardMinutes(210))); //03:30-04:00
-        basals.add(new BasalScheduleEntry(4, Duration.standardMinutes(240)));    //04:00-04:30
-        basals.add(new BasalScheduleEntry(14.5, Duration.standardMinutes(270))); //04:30-05:00
-        basals.add(new BasalScheduleEntry(5, Duration.standardMinutes(300)));    //05:00-05:30
-        basals.add(new BasalScheduleEntry(15.5, Duration.standardMinutes(330))); //05:30-06:00
-        basals.add(new BasalScheduleEntry(6, Duration.standardMinutes(360)));    //06:00-06:30
-        basals.add(new BasalScheduleEntry(16.5, Duration.standardMinutes(390))); //06:30-07:00
-        basals.add(new BasalScheduleEntry(7, Duration.standardMinutes(420)));    //07:00-07:30
-        basals.add(new BasalScheduleEntry(17.5, Duration.standardMinutes(450))); //07:30-08:00
-        basals.add(new BasalScheduleEntry(8, Duration.standardMinutes(480)));    //08:00-08:30
-        basals.add(new BasalScheduleEntry(18.5, Duration.standardMinutes(510))); //08:30-09:00
-        basals.add(new BasalScheduleEntry(9, Duration.standardMinutes(540)));    //09:00-09:30
-        basals.add(new BasalScheduleEntry(19.5, Duration.standardMinutes(570))); //09:30-10:00
-        basals.add(new BasalScheduleEntry(10, Duration.standardMinutes(600)));   //10:00-10:30
+        basals.add(new BasalScheduleEntry(1.5, Duration.standardMinutes(150))); //02:30-03:00
+        basals.add(new BasalScheduleEntry(0.85, Duration.standardMinutes(180)));    //03:00-03:30
+        basals.add(new BasalScheduleEntry(0.7, Duration.standardMinutes(210))); //03:30-04:00
+        basals.add(new BasalScheduleEntry(1, Duration.standardMinutes(240)));    //04:00-04:30
+        basals.add(new BasalScheduleEntry(1.5, Duration.standardMinutes(270))); //04:30-05:00
+        basals.add(new BasalScheduleEntry(0.55, Duration.standardMinutes(300)));    //05:00-05:30
+        basals.add(new BasalScheduleEntry(1.5, Duration.standardMinutes(330))); //05:30-06:00
+        basals.add(new BasalScheduleEntry(1.3, Duration.standardMinutes(360)));    //06:00-06:30
+        basals.add(new BasalScheduleEntry(0.65, Duration.standardMinutes(390))); //06:30-07:00
+        basals.add(new BasalScheduleEntry(0.8, Duration.standardMinutes(420)));    //07:00-07:30
+        basals.add(new BasalScheduleEntry(0.5, Duration.standardMinutes(450))); //07:30-08:00
+        basals.add(new BasalScheduleEntry(1.45, Duration.standardMinutes(480)));    //08:00-08:30
+        basals.add(new BasalScheduleEntry(1.1, Duration.standardMinutes(510))); //08:30-09:00
+        basals.add(new BasalScheduleEntry(0.3, Duration.standardMinutes(540)));    //09:00-09:30
+        basals.add(new BasalScheduleEntry(1.5, Duration.standardMinutes(570))); //09:30-10:00
+        basals.add(new BasalScheduleEntry(1, Duration.standardMinutes(600)));   //10:00-10:30
         basals.add(new BasalScheduleEntry(1.05, Duration.standardMinutes(630))); //10:30-11:00
-        basals.add(new BasalScheduleEntry(11, Duration.standardMinutes(660)));//11:00-22:00
+        basals.add(new BasalScheduleEntry(0.85, Duration.standardMinutes(660)));//11:00-22:00
         basals.add(new BasalScheduleEntry(1.15, Duration.standardMinutes(1320)));//22:00-24:00
         return new BasalSchedule(basals);
     }
